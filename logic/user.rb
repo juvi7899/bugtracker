9d902ba2f3de4c6fd2de97b4648a7409d3abdc73
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-03-23 21:16:49 +0200

    adding password hashing on change

--- logic/user.rb
+++ logic/user.rb
@@ -1,12 +1,20 @@
 require 'digest/sha1'
 
 class User
-  attr_accessor :name, :password, :type
+  attr_accessor :name, :type
 
   def initialize(name, password, type)
     @name = name
     @password = Digest::SHA1.hexdigest(password)
     @type = type
+  end
+
+  def password
+    @password
+  end
+
+  def password=(password)
+    @password = Digest::SHA1.hexdigest(password)
   end
 end
 
