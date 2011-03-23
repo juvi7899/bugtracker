a4cdc7cea7277c9c7317843d9afb6f37ccaa35ef
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-03-24 00:03:20 +0200

    implemented file attachment check

--- logic/comment.rb
+++ logic/comment.rb
@@ -5,4 +5,16 @@ class Comment
     @name = name
     @text = text
   end
+
+  def file
+    @file
+  end
+
+  def file=(file_name)
+    if File.exist? file_name
+      @file = file_name
+    else
+      raise Errno::ENOENT, "No such file or directory - #{file_name}"
+    end
+  end
 end
