857fe7f30ab03c23af0273de2a92c4f40b2382b6
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-03-23 23:56:52 +0200

    adding comment attachment test

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -1,4 +1,5 @@
 require '../logic/comment'
+require 'tmpdir'
 
 describe Comment, "when first created" do
   before(:each) do
@@ -11,5 +12,16 @@ describe Comment, "when first created" do
 
   it "should have a correct comment text" do
     @comment.text.should eql('Lorem Ipsum')
+  end
+end
+
+describe Comment, "when modified" do
+  before(:each) do
+    @comment = Comment.new('julius', 'Lorem Ipsum')
+  end
+
+  it "should raise an exception when adding not existing file" do
+    FileUtils.remove_file("#{Dir.tmpdir}/comment_spec.test", true)
+    lambda { @comment.file = "#{Dir.tmpdir}/comment_spec.test" }.should raise_exception(Errno::ENOENT)
   end
 end
