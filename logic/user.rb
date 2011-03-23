588db70eac111d13de5de111f4a5f6e6cd0a0458
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-03-23 20:12:51 +0200

    added user password

--- logic/user.rb
+++ logic/user.rb
@@ -1,8 +1,12 @@
-class User
-  attr_accessor :name, :type
+require 'digest/sha1'
 
-  def initialize(name, type)
+class User
+  attr_accessor :name, :password, :type
+
+  def initialize(name, password, type)
     @name = name
+    @password = Digest::SHA1.hexdigest(password)
     @type = type
   end
 end
+
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,17 @@
 require '../logic/user'
+require 'digest/sha1'
 
 describe User, "when first created" do
   before(:each) do
-    @user = User.new('julius', :admin)
+    @user = User.new('julius', 'secret', :admin)
   end
 
   it "should have a correct name" do
     @user.name.should eql('julius')
+  end
+
+  it "should have a correct password hash" do
+    @user.password.should eql('e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4')
   end
 
   it "should have a correct type" do
