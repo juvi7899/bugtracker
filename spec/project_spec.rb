39dca4ecff70ae2c42562891f0c1c9769af392a8
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-04-28 12:11:16 +0300

    changing project spec

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,18 +2,18 @@ require './logic/project'
 
 describe Project, "when first created" do
   before(:each) do
-    @project = Project.new('project R', 'Test project') 
+    @projects = Project.new('project R', 'Test project') 
   end
 
   it "should have a correct name" do
-    @project.name.should eql('project R')
+    @projects.name.should eql('project R')
   end
 
   it "should have a correct description" do
-    @project.description.should eql('Test project')
+    @projects.description.should eql('Test project')
   end
 
   it "should have zero bugs assigned" do
-    @project.bugs.should be_empty
+    @projects.bugs.should be_empty
   end
 end
