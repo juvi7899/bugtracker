c1d63e7d5d41ca910330b314152bda8c848cb7ed
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-04-28 12:16:22 +0300

    changing comment spec

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -3,35 +3,35 @@ require 'tmpdir'
 
 describe Comment, "when first created" do
   before(:each) do
-    @comment = Comment.new('julius', 'Lorem Ipsum')
+    @comments = Comment.new(:name => 'julius', :text => 'Lorem Ipsum')
   end
 
   it "should have a commenter's name" do
-    @comment.name.should eql('julius')
+    @comments.name.should eql('julius')
   end
 
   it "should have a correct comment text" do
-    @comment.text.should eql('Lorem Ipsum')
+    @comments.text.should eql('Lorem Ipsum')
   end
 
   it "should have no files attached" do
-    @comment.file.should be_nil
+    @comments.file.should be_nil
   end
 end
 
 describe Comment, "when modified" do
   before(:each) do
-    @comment = Comment.new('julius', 'Lorem Ipsum')
+    @comments = Comment.new(:name => 'julius', :text => 'Lorem Ipsum')
   end
 
   it "should raise an exception when adding non-existing file" do
     FileUtils.remove_file("#{Dir.tmpdir}/comment_spec.test", true)
     lambda {
-      @comment.file = "#{Dir.tmpdir}/comment_spec.test"
+      @comments.file = "#{Dir.tmpdir}/comment_spec.test"
     }.should raise_exception(Errno::ENOENT)
   end
 
   it "should add a file successfully" do
-    @comment.file = "/home/julius/bugtracker/README"
+    @comments.file = "/home/julius/bugtracker/README"
   end
 end
