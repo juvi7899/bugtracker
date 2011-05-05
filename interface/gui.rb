706f43ed4865ef553a350e8ebade71c0cd945846
Julius Vitkauskas <Julius.Vitkauskas@mif.stud.vu.lt>
2011-05-05 14:39:14 +0300

    deleting interface

--- interface/gui.rb
+++
@@ -1,7 +0,0 @@
-require 'Qt4'
-require './interface/loginwindow'
-
-application = Qt::Application.new(ARGV)
-login_window = LoginWindow.new
-login_window.show
-application.exec
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-env ruby interface/gui.rb
